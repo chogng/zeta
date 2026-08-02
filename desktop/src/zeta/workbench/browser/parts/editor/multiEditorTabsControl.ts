0b9fdebebf268b3a9799d838e78247ae4a80850d
chenchen <56059950+chogng@users.noreply.github.com>
2026-08-03 01:10:05 +0800

    refactor: move tab control styles to media

--- desktop/src/zeta/workbench/browser/parts/editor/multiEditorTabsControl.ts
+++ desktop/src/zeta/workbench/browser/parts/editor/multiEditorTabsControl.ts
@@ -1,4 +1,4 @@
-import "./multiEditorTabsControl.css";
+import "./media/multiEditorTabsControl.css";
 import { DataTransfers } from "../../../../base/browser/dnd.js";
 import { TabList } from "../../../../base/browser/ui/tablist/tabList.js";
 import { containsExternalEditorDrop } from "./editorDropData.js";
